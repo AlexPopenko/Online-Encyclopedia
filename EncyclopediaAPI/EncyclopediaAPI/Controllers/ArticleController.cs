17defb35e9fa409b0d425efccb6edad8257b7c4b
Alexandr <93203382+AlexPopenko@users.noreply.github.com>
2024-04-06 03:50:22 -0400

    Worked on API

---
+++ EncyclopediaAPI/EncyclopediaAPI/Controllers/ArticleController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using 
+namespace EncyclopediaAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ArticleController : ControllerBase
+    {
+        
+            
+
+            private readonly ILogger<ArticleController> _logger;
+
+            public ArticleController(ILogger<ArticleController> logger)
+            {
+                _logger = logger;
+            }
+
+            [HttpGet(Name = "GetArticles")]
+            public IEnumerable<Models.Article> Get()
+            {
+                return 
+                    Id = id;
+                    Name = name;
+                    Tag = tag;
+                    Creator = creator;
+                    CreationDate = Models.Article.SetCreationDate();
+                    Content = content;
+            })
+                .ToArray();
+            }
+        }
+    
+}
